8decbbe6295b28508e56dba6bbcb7306964790c2
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2025-02-18 01:55:31 -0800

    Final commit before 1.0 hopefully...

--- src/main/java/org/mangorage/mangobotsite/MangoBotSite.java
+++ src/main/java/org/mangorage/mangobotsite/MangoBotSite.java
@@ -2,18 +2,16 @@ package org.mangorage.mangobotsite;
 
 import org.mangorage.mangobot.MangoBotPlugin;
 import org.mangorage.mangobotapi.core.plugin.PluginManager;
-import org.mangorage.mangobotapi.core.plugin.api.AddonPlugin;
+import org.mangorage.mangobotapi.core.plugin.api.AbstractPlugin;
 import org.mangorage.mangobotapi.core.plugin.impl.Plugin;
 import org.mangorage.mangobotsite.website.WebServer;
 import org.mangorage.mangobotsite.website.impl.ObjectMap;
 
-@Plugin(id = MangoBotSite.ID, type = Plugin.Type.ADDON)
-public final class MangoBotSite extends AddonPlugin {
+@Plugin(id = MangoBotSite.ID)
+public final class MangoBotSite extends AbstractPlugin {
     public static final String ID = "mangobotsite";
 
     public MangoBotSite() {
-        super(ID);
-
         var pl = PluginManager.getPlugin("mangobot", MangoBotPlugin.class);
 
         ObjectMap objectMap = new ObjectMap();
