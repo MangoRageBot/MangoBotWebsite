4e4f0270782f55ce32c689d13f8f3ca9f149e4f7
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2025-03-21 23:36:44 -0700

    Minor Fixes

--- src/main/java/org/mangorage/mangobotsite/website/impl/StandardHttpServlet.java
+++ src/main/java/org/mangorage/mangobotsite/website/impl/StandardHttpServlet.java
@@ -33,7 +33,7 @@ public abstract class StandardHttpServlet extends HttpServlet {
     }
 
     public String getStyles() {
-        return useDefaultStyles() ? "css/styles.css" : "%s.css".formatted(getServletInfo());
+        return useDefaultStyles() ? "css/styles.css" : "css/%s.css".formatted(getServletInfo());
     }
 
     @Override
